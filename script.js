49581c12b58b5286ddc6a9f5e3653207c4e741b9
nasirmasud <nasir.masud@ymail.com>
2026-02-23 22:03:19 +0600

    Added interview button event listener & interview render card

--- script.js
+++ script.js
@@ -92,3 +92,87 @@ function toggleStyle(id) {
     }
   }
 }
+
+// interview button event listener
+mainJobCardList.addEventListener("click", function (e) {
+  if (e.target.classList.contains("interview-btn")) {
+    const parentNode = e.target.parentNode.parentNode;
+    const companyName = parentNode.querySelector(".company-name").innerText;
+    const jobRole = parentNode.querySelector(".job-role").innerText;
+    const jobDetails = parentNode.querySelector(".job-details").innerText;
+    const roleDetails = parentNode.querySelector(".role-details").innerText;
+    const status = parentNode.querySelector(".status").innerText;
+
+    parentNode.querySelector(".status").innerText = "INTERVIEW";
+
+    const jobData = {
+      companyName,
+      jobRole,
+      jobDetails,
+      roleDetails,
+      status: "INTERVIEW",
+    };
+
+    const jobExists = interviewList.find(
+      (item) => item.companyName == jobData.companyName,
+    );
+    if (!jobExists) {
+      interviewList.push(jobData);
+    }
+
+    totalJobCount();
+    renderJobCard();
+  }
+});
+
+// render interview job card
+function renderJobCard() {
+  filteredJobCardList.innerHTML = "";
+
+  for (jobs of interviewList) {
+    let div = document.createElement("div");
+    div.className =
+      "job-card grid grid-cols-1 sm:grid-cols-12 gap-4 items-start bg-white rounded-2xl p-6";
+    div.innerHTML = `
+              <div class="card-content col-span-1 sm:col-span-11 order-1">
+            <h4 class="company-name text-xl font-bold text-[#002c5c]">
+              ${jobs.companyName}
+            </h4>
+            <h5 class="job-role text-lg text-gray-600">
+              ${jobs.jobRole}
+            </h5>
+            <p class="job-details text-sm text-gray-600 py-4">
+              ${jobs.jobDetails}
+            </p>
+            <button
+              class="status bg-[#eef4ff] font-semibold py-2 px-4 border-0 rounded text-sm"
+            >
+              ${jobs.status}
+            </button>
+            <p class="role-details text-xs text-gray-600 py-4">${jobs.roleDetails}</p>
+            <div class="job-status flex flex-wrap gap-2">
+              <button
+                class="bg-transparent hover:bg-green-500 text-green-600 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded text-sm"
+              >
+                INTERVIEW
+              </button>
+              <button
+                class="bg-transparent hover:bg-red-500 text-red-600 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded text-sm"
+              >
+                REJECTED
+              </button>
+            </div>
+          </div>
+          <div
+            class="card-delete col-span-1 sm:col-span-1 flex justify-start sm:justify-end order-2"
+          >
+            <button
+              class="bg-transparent hover:bg-red-500 text-gray-600 hover:text-white px-3 py-2 border border-gray-400 rounded-full"
+            >
+              <i class="fa-regular fa-trash-can"></i>
+            </button>
+          </div>`;
+
+    filteredJobCardList.appendChild(div);
+  }
+}
